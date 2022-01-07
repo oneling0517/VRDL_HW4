113c38312fa411fd84980130f7f41b94dde62f61
oneling0517 <63098487+oneling0517@users.noreply.github.com>
2022-01-07 16:14:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,16 +15,19 @@ srresnet = torch.load(srresnet_checkpoint)['model'].to(device)
 srresnet.eval()
 model = srresnet
 
-test_dataset = SRDataset(split='test', crop_size=0, scaling_factor=3, lr_img_type='imagenet-norm',
-                         hr_img_type='[-1, 1]')
+test_dataset = SRDataset(split = 'test', crop_size = 0, scaling_factor = 1 / 3, lr_img_type = 'imagenet-norm',
+                         hr_img_type = '[-1, 1]')
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4,
-                                          pin_memory=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False, num_workers = 4,
+                                          pin_memory = True)
 
 # Save the images in the folder
 with torch.no_grad():
+    output = []
     for i, (lr_imgs, hr_imgs, image_path) in enumerate(test_loader):
         lr_imgs = lr_imgs.to(device)
         sr_imgs = convert_image(model(lr_imgs).squeeze(0).cpu().detach(), '[-1, 1]', 'pil')
-
-        sr_imgs.save(os.path.join(output_dir, image_path[0].split('/')[-1]))
+        
+        output.append(image_path[0].split('/')[-1])
+        sr_imgs.save(os.path.join(output_dir, output[i][:-4] + "_pred" + output[i][-4:]))
+        #print(output[i][:-4] + "_pred" + output[i][-4:])
