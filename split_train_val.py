877110fdbfd7d0084e6a6f43fc11fada8c96a114
oneling0517 <63098487+oneling0517@users.noreply.github.com>
2022-01-07 16:59:34 +0800

    Update split_train_val.py

--- split_train_val.py
+++ split_train_val.py
@@ -1,6 +1,5 @@
 import os
-
-from sklearn.model_selection import train_test_split
+import numpy as np
 from PIL import Image
 
 root_total = '/content/VRDL_HW4/dataset/training_hr_images/training_hr_images'
@@ -15,16 +14,27 @@ if not os.path.isdir(root_val):
 
 images = []
 
+# Training proportion
+split_proportion = 0.8
+
 # Read list of image-paths
 for dir_path, dir_names, file_names in os.walk(root_total):
     for f in file_names:
         images.append(os.path.join(dir_path, f))
+#print(len(images))
 
-train_img, val_img = train_test_split(images, test_size=0.2)
+nbr_train = int(len(images) * split_proportion)
+np.random.shuffle(images)
+train_images = images[:nbr_train]
+val_images = images[nbr_train:]
 
-for img_path in train_img:
+#train_img, val_img = train_test_split(images, test_size=0.2)
+#print(len(train_img))
+
+for img_path in train_images:
     img = Image.open(img_path)
     img.save(os.path.join(root_train, img_path.split('/')[-1]))
-for img_path in val_img:
+for img_path in val_images:
     img = Image.open(img_path)
     img.save(os.path.join(root_val, img_path.split('/')[-1]))
+
