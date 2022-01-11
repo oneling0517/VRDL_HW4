fe7e454fcd5e2258514663b122a71a57dfabb41d
oneling0517 <63098487+oneling0517@users.noreply.github.com>
2022-01-11 15:52:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
+from PIL import Image
+import os
+import json
 import random
-
+import torchvision.transforms.functional as FT
 import numpy as np
 import torch
-import torchvision.transforms.functional as F
-from PIL import Image
+import math
 from torchvision import transforms
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -15,37 +17,93 @@ imagenet_std = torch.FloatTensor([0.229, 0.224, 0.225]).unsqueeze(1).unsqueeze(2
 imagenet_mean_cuda = torch.FloatTensor([0.485, 0.456, 0.406]).to(device).unsqueeze(0).unsqueeze(2).unsqueeze(3)
 imagenet_std_cuda = torch.FloatTensor([0.229, 0.224, 0.225]).to(device).unsqueeze(0).unsqueeze(2).unsqueeze(3)
 
-
-class AverageMeter(object):
+'''
+def create_data_lists(train_folders, test_folders, min_size, output_folder):
     """
-    Keeps track of most recent, average, sum, and count of a metric.
+    Create lists for images in the training set and each of the test sets.
+    :param train_folders: folders containing the training images; these will be merged
+    :param test_folders: folders containing the test images; each test folder will form its own test set
+    :param min_size: minimum width and height of images to be considered
+    :param output_folder: save data lists here
     """
+    print("\nCreating data lists... this may take some time.\n")
+    train_images = list()
+    for d in train_folders:
+        for i in os.listdir(d):
+            img_path = os.path.join(d, i)
+            img = Image.open(img_path, mode='r')
+            if img.width >= min_size and img.height >= min_size:
+                train_images.append(img_path)
+    print("There are %d images in the training data.\n" % len(train_images))
+    with open(os.path.join(output_folder, 'train_images.json'), 'w') as j:
+        json.dump(train_images, j)
 
-    def __init__(self):
-        self.count = 0
-        self.sum = 0
-        self.avg = 0
-        self.val = 0
+    for d in test_folders:
+        test_images = list()
+        test_name = d.split("/")[-1]
+        for i in os.listdir(d):
+            img_path = os.path.join(d, i)
+            img = Image.open(img_path, mode='r')
+            if img.width >= min_size and img.height >= min_size:
+                test_images.append(img_path)
+        print("There are %d images in the %s test data.\n" % (len(test_images), test_name))
+        with open(os.path.join(output_folder, test_name + '_test_images.json'), 'w') as j:
+            json.dump(test_images, j)
 
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+    print("JSONS containing lists of Train and Test images have been saved to %s\n" % output_folder)
+'''
 
-
-def clip_gradient(optimizer, grad_clip):
+def convert_image(img, source, target):
     """
-    Clips gradients computed during backpropagation to avoid explosion of gradients.
+    Convert an image from a source format to a target format.
 
-    :param optimizer: optimizer with the gradients to be clipped
-    :param grad_clip: clip value
+    :param img: image
+    :param source: source format, one of 'pil' (PIL image), '[0, 1]' or '[-1, 1]' (pixel value ranges)
+    :param target: target format, one of 'pil' (PIL image), '[0, 255]', '[0, 1]', '[-1, 1]' (pixel value ranges),
+                   'imagenet-norm' (pixel values standardized by imagenet mean and std.),
+                   'y-channel' (luminance channel Y in the YCbCr color format, used to calculate PSNR and SSIM)
+    :return: converted image
     """
-    for group in optimizer.param_groups:
-        for param in group['params']:
-            if param.grad is not None:
-                param.grad.data.clamp_(-grad_clip, grad_clip)
+    assert source in {'pil', '[0, 1]', '[-1, 1]'}, "Cannot convert from source format %s!" % source
+    assert target in {'pil', '[0, 255]', '[0, 1]', '[-1, 1]', 'imagenet-norm',
+                      'y-channel'}, "Cannot convert to target format %s!" % target
 
+    # Convert from source to [0, 1]
+    if source == 'pil':
+        img = FT.to_tensor(img)
+
+    elif source == '[0, 1]':
+        pass  # already in [0, 1]
+
+    elif source == '[-1, 1]':
+        img = (img + 1.) / 2.
+
+    # Convert from [0, 1] to target
+    if target == 'pil':
+        img = FT.to_pil_image(img)
+
+    elif target == '[0, 255]':
+        img = 255. * img
+
+    elif target == '[0, 1]':
+        pass  # already in [0, 1]
+
+    elif target == '[-1, 1]':
+        img = 2. * img - 1.
+
+    elif target == 'imagenet-norm':
+        if img.ndimension() == 3:
+            img = (img - imagenet_mean) / imagenet_std
+        elif img.ndimension() == 4:
+            img = (img - imagenet_mean_cuda) / imagenet_std_cuda
+
+    elif target == 'y-channel':
+        # Based on definitions at https://github.com/xinntao/BasicSR/wiki/Color-conversion-in-SR
+        # torch.dot() does not work the same way as numpy.dot()
+        # So, use torch.matmul() to find the dot product between the last dimension of an 4-D tensor and a 1-D tensor
+        img = torch.matmul(255. * img.permute(0, 2, 3, 1)[:, 4:-4, 4:-4, :], rgb_weights) / 255. + 16.
+
+    return img
 
 class ImageTransforms(object):
     """
@@ -75,6 +133,7 @@ class ImageTransforms(object):
         """
         if img.width <= self.crop_size or img.height <= self.crop_size:
             img = padding(img, self.crop_size)
+        
         # Crop
         if self.split == 'train':
             if img.width - self.crop_size <= 1:
@@ -129,58 +188,36 @@ class ImageTransforms(object):
         hr_img = convert_image(hr_img, source='pil', target=self.hr_img_type)
         return lr_img, hr_img
 
-
-def convert_image(img, source, target):
+class AverageMeter(object):
     """
-    Convert an image from a source format to a target format.
-
-    :param img: image
-    :param source: source format, one of 'pil' (PIL image), '[0, 1]' or '[-1, 1]' (pixel value ranges)
-    :param target: target format, one of 'pil' (PIL image), '[0, 255]', '[0, 1]', '[-1, 1]' (pixel value ranges),
-                   'imagenet-norm' (pixel values standardized by imagenet mean and std.),
-                   'y-channel' (luminance channel Y in the YCbCr color format, used to calculate PSNR and SSIM)
-    :return: converted image
+    Keeps track of most recent, average, sum, and count of a metric.
     """
-    assert source in {'pil', '[0, 1]', '[-1, 1]'}, "Cannot convert from source format %s!" % source
-    assert target in {'pil', '[0, 255]', '[0, 1]', '[-1, 1]', 'imagenet-norm',
-                      'y-channel'}, "Cannot convert to target format %s!" % target
 
-    # Convert from source to [0, 1]
-    if source == 'pil':
-        img = F.to_tensor(img)
+    def __init__(self):
+        self.count = 0
+        self.sum = 0
+        self.avg = 0
+        self.val = 0
 
-    elif source == '[0, 1]':
-        pass  # already in [0, 1]
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
-    elif source == '[-1, 1]':
-        img = (img + 1.) / 2.
 
-    # Convert from [0, 1] to target
-    if target == 'pil':
-        img = F.to_pil_image(img)
+def clip_gradient(optimizer, grad_clip):
+    """
+    Clips gradients computed during backpropagation to avoid explosion of gradients.
 
-    elif target == '[0, 255]':
-        img = 255. * img
+    :param optimizer: optimizer with the gradients to be clipped
+    :param grad_clip: clip value
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
 
-    elif target == '[0, 1]':
-        pass  # already in [0, 1]
-
-    elif target == '[-1, 1]':
-        img = 2. * img - 1.
-
-    elif target == 'imagenet-norm':
-        if img.ndimension() == 3:
-            img = (img - imagenet_mean) / imagenet_std
-        elif img.ndimension() == 4:
-            img = (img - imagenet_mean_cuda) / imagenet_std_cuda
-
-    elif target == 'y-channel':
-        # Based on definitions at https://github.com/xinntao/BasicSR/wiki/Color-conversion-in-SR
-        # torch.dot() does not work the same way as numpy.dot()
-        # So, use torch.matmul() to find the dot product between the last dimension of an 4-D tensor and a 1-D tensor
-        img = torch.matmul(255. * img.permute(0, 2, 3, 1)[:, 4:-4, 4:-4, :], rgb_weights) / 255. + 16.
-
-    return img
 
 
 def padding(img, crop_size):
@@ -190,4 +227,4 @@ def padding(img, crop_size):
     hp = int((max_h - h) / 2)
     wp = int((max_w - w) / 2)
     pad = [wp, hp, wp, hp]
-    return F.pad(img, pad, 0, 'constant')
+    return FT.pad(img, pad, 0, 'constant')
