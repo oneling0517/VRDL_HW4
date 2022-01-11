3f0eb987a41fbbecb961e094cef8b8f1c2a9a30b
oneling0517 <63098487+oneling0517@users.noreply.github.com>
2022-01-11 15:27:27 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -28,30 +28,8 @@ val_dataset = SRDataset(split='val', crop_size=0, scaling_factor=3, lr_img_type=
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4,
                                          pin_memory=True)
 
+PSNRs = AverageMeter()
 
-def rgb2ycbcr(im):
-    cbcr = np.empty_like(im)
-    r = im[:, :, 0]
-    g = im[:, :, 1]
-    b = im[:, :, 2]
-    # Y
-    cbcr[:, :, 0] = .299 * r + .587 * g + .114 * b
-    # Cb
-    cbcr[:, :, 1] = 128 - .169 * r - .331 * g + .5 * b
-    # Cr
-    cbcr[:, :, 2] = 128 + .5 * r - .419 * g - .081 * b
-    return np.uint8(cbcr)
-
-
-def cal_psnr(sr_img, hr_img):
-    sr_img = cv2.cvtColor(np.asarray(sr_img), cv2.COLOR_RGB2BGR)
-    hr_img = cv2.cvtColor(np.asarray(hr_img), cv2.COLOR_RGB2BGR)
-
-    sr_img_y = rgb2ycbcr(sr_img)[:, :, 0]
-    hr_img_y = rgb2ycbcr(hr_img)[:, :, 0]
-    return peak_signal_noise_ratio(hr_img_y, sr_img_y)
-
-psnrs = []
 with torch.no_grad():
     # Batches
     for i, (lr_imgs, hr_imgs) in enumerate(val_loader):
@@ -63,9 +41,10 @@ with torch.no_grad():
         sr_imgs = model(lr_imgs)
 
         # Calculate PSNR
-        sr_img = convert_image(sr_imgs.squeeze(0), source='[-1, 1]', target='pil')
-        hr_img = convert_image(hr_imgs.squeeze(0), source='[-1, 1]', target='pil')
-
-        psnrs.append(cal_psnr(hr_img, sr_img))
-
-print(f'Model images PSNR: {np.mean(psnrs): .3f}')
+        sr_img = convert_image(sr_imgs, source='[-1, 1]', target='y-channel').squeeze(0)  # (w, h), in y-channel
+        hr_img = convert_image(hr_imgs, source='[-1, 1]', target='y-channel').squeeze(0)  # (w, h), in y-channel
+        psnr = peak_signal_noise_ratio(hr_img.cpu().numpy(), sr_img.cpu().numpy(),data_range=255.)
+      
+        PSNRs.update(psnr, lr_imgs.size(0))
+        
+print('PSNR - {psnrs.avg:.3f}'.format(psnrs=PSNRs))
